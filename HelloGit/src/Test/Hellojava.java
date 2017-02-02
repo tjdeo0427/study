f68a0e77c81d9e1bda7237ffcebc862f45d81c96
acorn <acorn@acorn-PC>
2017-02-02 10:46:56 +0900

    create java project first commit work

---
+++ HelloGit/src/Test/Hellojava.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Forza Suwon~!");
+		System.out.println("Wins goal~yeah~~");
+		
+		System.out.println("Hello java~~");
+		System.out.println("First commit work");
+	}
+
+}
